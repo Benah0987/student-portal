a354a583f9d9d38cfef18d6d739a449e8aee70c7
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2025-07-02 18:52:13 +0300

    added pages for the student, added assets

--- Home/school/views.py
+++ Home/school/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse  # ✅ Import this
 # Create your views here.
 
 def index(request):  # ✅ Use 'request' instead of 'report'
-    return HttpResponse("Hello world")
+    return render(request, "Home/index.html")
