3fce406f68ce18a7818a1bd1e026a3725f4b3e68
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2025-07-09 13:51:40 +0300

    added the admin succesfully

--- Home/Home/urls.py
+++ Home/Home/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("school.urls"))
-
+    path('', include("school.urls")),
+    path('student/', include("student.urls")),  # you can namespace this path
 ]
+
---
+++ Home/student/urls.py
@@ -0,0 +1,8 @@
+# student/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='student_index'),
+]
