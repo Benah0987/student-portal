ba97fb817e9dfedbb218d1088c73fc906993457e
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2025-07-12 06:51:57 +0300

    added the admin succesfully

--- Home/student/urls.py
+++ Home/student/urls.py
@@ -1,8 +1,10 @@
-# student/urls.py
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='student_index'),
+    path('', views.student_list, name='student_list'),
+     path('add-student/', views.add_student, name='add-student'),
+    path('add-student/', views.add_student, name='add-student'),
+    path('edit-student/', views.edit_student, name='edit-student'),
+    path('student-details/', views.view_student, name='view-student'),
 ]
--- Home/student/views.py
+++ Home/student/views.py
@@ -1,4 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Hello from the Student app.")
+def student_list(request):
+    return render(request, "students/students.html")
+
+def add_student(request):
+    return render(request, "students/add-student.html")
+
+def edit_student(request):
+    return render(request, "students/edit-student.html")
+
+def view_student(request):
+    return render(request, "students/view-student.html")
