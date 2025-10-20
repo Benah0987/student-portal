931ba547460903119d999e470663a8d2e12aa515
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2025-10-20 09:14:53 +0300

    added a docker image

--- Home/school/urls.py
+++ Home/school/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
 
     # Admin
     path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
+
+    path('notification/mark-as-read/', views.mark_notification_as_read, name='mark_notification_as_read' ),
+   path('notification/clear-all', views.clear_all_notification, name= "clear_all_notification")
+
 ]
--- Home/school/views.py
+++ Home/school/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+
+from .models import Notification  # ✅ Correct import — no circular import
+
 
 def index(request):
     return render(request, "Home/index.html")
 
 
-def dashboard(request):  # Student dashboard
+def dashboard(request):
+    unread_notification = Notification.objects.filter(user=request.user, is_read=False)
+    unread_notification_count = unread_notification.count()
     user = request.user
     if not user.is_student:
         return redirect('login')  # prevent non-students from viewing
@@ -24,3 +30,17 @@ def admin_dashboard(request):  # Admin dashboard
     if not user.is_admin:
         return redirect('login')
     return render(request, "admins/admin-dashboard.html", {'user': user})
+
+def mark_notification_as_read(request):
+    if request.method == 'POST':
+        notification = Notification.objects.filter(user=request.user, is_read=False)
+        notification.update(is_read=True)
+        return JsonResponse({'status': 'success'})
+    return HttpResponseForbidden()
+
+def clear_all_notification(request):
+    if request.method == "POST":
+        notification = Notification.objects.filter(user=request.user)
+        notification.delete()
+        return JsonResponse({'status': 'success'})
+    return HttpResponseForbidden
